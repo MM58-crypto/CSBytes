e99466ab9a3853c13c24f346d69fde308918b77f
MM58 <62597737+MM58-crypto@users.noreply.github.com>
2025-01-24 11:08:58 +0300

    another commit, wooo

--- CS2.py
+++ CS2.py
@@ -3,10 +3,11 @@ import pathlib
 import textwrap
 import google.generativeai as genai
 import time
-#import config
+import config
 from fpdf import FPDF
 
-gemini_key = st.secrets["gemini_api_key"] 
+gemini_key = config.GEMINI_API_KEY
+#st.secrets["gemini_api_key"]
 #config.GEMINI_API_KEY
 genai.configure(api_key=gemini_key)
 model = genai.GenerativeModel('gemini-pro')
@@ -32,26 +33,56 @@ tab1, tab2 = st.tabs(['PRG Langs', 'CS Topics'])
 
 with tab1:
     st.header('PRG Langs')
-    prg_langs = ['C','C++', 'Python', 'Java', 'Javascript', 'Haskell', 'Common Lisp','Clojure','Golang', 'Other']
-    prg_choices = st.radio("Programming Languages", prg_langs)
+    st.write("Select the type of programming language")
+
+
+    functional_prg_langs = ['Haskell', 'Common Lisp', 'Clojure', 'Scala', 'Elixir', 'Racket', 'Erlang', 'F#', 'Scheme' , 'OCaml']
+    imperative_prg_langs = ['C','C++', 'Python', 'Java', 'Javascript', 'Swift', 'Go', 'Ruby', 'Kotlin', 'Rust']
+    prg_type = st.radio(
+        "Programming Language Type",
+        ["Functional", "Imperative"]
+    )
+
+    # input
+    functional_prg_choices = st.selectbox("Functional Programming Languages", functional_prg_langs)
+    imperative_prg_choices = st.selectbox("Imperative/OOP Programming Languages", imperative_prg_langs)
+    # prg_langs = ['C','C++', 'Python', 'Java', 'Javascript', 'Haskell', 'Common Lisp','Clojure','Golang']
+    # prg_choices = st.radio("Programming Languages", prg_langs)
     pnum_pages = st.selectbox('Pages',
                             ['1 page', '2 pages', '3 pages (max)'])
-    prg_prompt = f"Generate a descriptive cheat sheet on {prg_choices} (should include code snippets for examples or use cases)and the sheet should be {pnum_pages} in markdown format"
+    func_prg_prompt = f"Generate a descriptive cheat sheet on {functional_prg_choices} (should include code snippets for examples or use cases)and the sheet should be {pnum_pages} in markdown format"
+    imperative_prg_prompt = f"Generate a descriptive cheat sheet on {imperative_prg_choices} (should include code snippets for examples or use cases)and the sheet should be {pnum_pages} in markdown format"
 
     generate_btn = st.button('Generate Sheet')
+# define a func for the generation code block
 if generate_btn:
-    with st.spinner("Generating..."):
-        time.sleep(35)
-    response = model.generate_content(prg_prompt)
-    pdf_file = generate_pdf(response.text)
-    st.success(response.text)
-    with open(pdf_file, "rb") as file:
-        st.download_button(
-            label='Download Sheet',
-            data=file,
-            file_name=f'{prg_choices} Cheat-Sheet.pdf',
-            mime='application/pdf'
-        )
+    if prg_type == "Functional":
+        with st.spinner("Generating..."):
+            time.sleep(45)
+            response = model.generate_content(func_prg_prompt)
+            pdf_file = generate_pdf(response.text)
+            st.success(response.text)
+            with open(pdf_file, "rb") as file:
+                st.download_button(
+                    label='Download Sheet',
+                    data=file,
+                    file_name=f'{functional_prg_choices} Cheat-Sheet.pdf',
+                    mime='application/pdf'
+                )
+    elif prg_type == "Imperative":
+         with st.spinner("Generating..."):
+            time.sleep(45)
+            response = model.generate_content(imperative_prg_prompt)
+            pdf_file = generate_pdf(response.text)
+            st.success(response.text)
+            with open(pdf_file, "rb") as file:
+                st.download_button(
+                    label='Download Sheet',
+                    data=file,
+                    file_name=f'{imperative_prg_choices} Cheat-Sheet.pdf',
+                    mime='application/pdf'
+                )
+
 with tab2:
     st.header('CS Topics')
     cs_topics = ['Computer Architecture', 'Data Structures & Algorithms', 'Operating Systems', 'Distributed Systems', 'Computer Networking', 'Databases' ,'Other'] # add or del elemnts to list later
@@ -61,6 +92,7 @@ with tab2:
     cs_prompt = f"Generate a descriptive cheat sheet on this topic {topics_list} and the sheet should be {cnum_pages} in markdown format"
 
     generate_btn2 = st.button('Generate CS Sheet')
+
 if generate_btn2:
     with st.spinner("Generating..."):
         time.sleep(35)
